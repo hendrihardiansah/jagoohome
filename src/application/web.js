99f35bb6c2c8bdee8c4874b6914c92d64584ab51
hendrihardiansah <155897701+hendrihardiansah@users.noreply.github.com>
2024-08-09 23:41:56 +0700

    update

--- src/application/web.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { jagoohomePublicRouter } from "../route/public_api.js";
-import { errorMiddleware } from "../middleware/error-middleware.js";
-
-export const web = express ();
-web.use(express.json());
-
-web.use(jagoohomePublicRouter);
-web.use(errorMiddleware);
